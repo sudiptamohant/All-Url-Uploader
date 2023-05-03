2b48c0d83554568729dd0428cb5532c548d9d9d3
sudiptamohant <78973261+sudiptamohant@users.noreply.github.com>
2023-05-04 01:46:33 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -25,11 +25,11 @@
 class Config(object):
 
     # get a token from @BotFather
-    BOT_TOKEN = ""
+    BOT_TOKEN = "6118812161:AAHCZrTJnGNpIfxGOmzkjGhEP1c5231kYJ0"
 
     # Get these values from my.telegram.org
-    API_ID = 1234567
-    API_HASH = ""
+    API_ID = 5806640
+    API_HASH = "127f130ad3745dbcd31aa39aa0eabcb8"
 
     # No need to change
     DOWNLOAD_LOCATION = "./DOWNLOADS"
@@ -40,11 +40,11 @@ class Config(object):
     PROCESS_MAX_TIMEOUT = 3700
 
     # TG Ids
-    LOG_CHANNEL = -1007119867241
-    OWNER_ID = 1288398723
+    LOG_CHANNEL = -1001981767301
+    OWNER_ID = 1375408229
 
     # bot username without @
-    BOT_USERNAME = "AdvanceUrlUploaderBot"
+    BOT_USERNAME = "Autoleexmcherbot"
 
     # auth users
     AUTH_USERS = [OWNER_ID]
